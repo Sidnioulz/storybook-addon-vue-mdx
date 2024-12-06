acfdd196a7a60db54e632869e60c47e7494976d2
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-12-06 09:46:09 +0100

    fix: Support components without a scope ID

---
+++ src/components/StylelessButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+const emit = defineEmits(['click'])
+const onClick = () => emit('click')
+</script>
+
+<template>
+  <button type="button" @click="onClick">
+    <slot />
+  </button>
+</template>
